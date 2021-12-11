53a78f15aa3f673964fb533370a920380bd916f8
truongnguyenlinh <ltruong28@my.bcit.ca>
2021-12-10 17:07:02 -0800

    Remove unnecessary xml write function

--- track.py
+++ track.py
@@ -45,30 +45,6 @@ def haversine(lon1, lat1, lon2, lat2, earth_radius=6371):
     return earth_radius * 2 * np.arcsin(np.sqrt(a))
 
 
-def output_gpx(points, output_filename):
-    """
-    Output a GPX file with latitude and longitude from the points DataFrame.
-    """
-    from xml.dom.minidom import getDOMImplementation
-
-    def append_trkpt(pt, trkseg, doc):
-        trkpt = doc.createElement('trkpt')
-        trkpt.setAttribute('lat', '%.8f' % (pt['lat']))
-        trkpt.setAttribute('lon', '%.8f' % (pt['lon']))
-        trkseg.appendChild(trkpt)
-    
-    doc = getDOMImplementation().createDocument(None, 'gpx', None)
-    trk = doc.createElement('trk')
-    doc.documentElement.appendChild(trk)
-    trkseg = doc.createElement('trkseg')
-    trk.appendChild(trkseg)
-    
-    points.apply(append_trkpt, axis=1, trkseg=trkseg, doc=doc)
-    
-    with open(output_filename, 'w') as fh:
-        doc.writexml(fh, indent=' ')
-
-
 def get_data(in_directory, amenity_schema=amenity_schema):
     vancouver_data = spark.read.json(in_directory, schema=amenity_schema)
     vancouver_data = vancouver_data.na.drop(subset=['name'])
