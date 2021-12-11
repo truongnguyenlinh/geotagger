0ae38b9e6e68080e227145cf75dc98c4e0a7278e
truongnguyenlinh <ltruong28@my.bcit.ca>
2021-12-10 16:59:27 -0800

    Add EDA function

--- track.py
+++ track.py
@@ -1,10 +1,17 @@
 import sys
-assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
+import numpy as np
+import pandas as pd
+import os
+from xml.dom.minidom import parse
+import matplotlib.pyplot as plt
+from collections import Counter
+from pyspark.sql import SparkSession, functions, types, Row, Window
 
-from pyspark.sql import SparkSession, functions, types, Row
+assert sys.version_info >= (3, 5) # make sure we have Python 3.5+
 spark = SparkSession.builder.appName('OSM point of interest extracter').getOrCreate()
 assert spark.version >= '2.4' # make sure we have Spark 2.4+
 spark.sparkContext.setLogLevel('WARN')
+spark.conf.set("spark.sql.session.timeZone", "UTC")
 
 
 amenity_schema = types.StructType([
@@ -17,23 +24,90 @@ amenity_schema = types.StructType([
 ])
 
 
+def haversine(lon1, lat1, lon2, lat2, earth_radius=6371):
+    """
+    Modified from https://stackoverflow.com/questions/43577086/pandas-calculate-haversine-distance-within-each-group-of-rows/43577275
+    :param earth_radius: an int
+    :param lon1: a float
+    :param lat1: a float
+    :param lon2: a float
+    :param lat2: a float
+    :return:
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2) ** 2 + \
+        np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0) ** 2
+
+    return earth_radius * 2 * np.arcsin(np.sqrt(a))
+
+
+def output_gpx(points, output_filename):
+    """
+    Output a GPX file with latitude and longitude from the points DataFrame.
+    """
+    from xml.dom.minidom import getDOMImplementation
+
+    def append_trkpt(pt, trkseg, doc):
+        trkpt = doc.createElement('trkpt')
+        trkpt.setAttribute('lat', '%.8f' % (pt['lat']))
+        trkpt.setAttribute('lon', '%.8f' % (pt['lon']))
+        trkseg.appendChild(trkpt)
+    
+    doc = getDOMImplementation().createDocument(None, 'gpx', None)
+    trk = doc.createElement('trk')
+    doc.documentElement.appendChild(trk)
+    trkseg = doc.createElement('trkseg')
+    trk.appendChild(trkseg)
+    
+    points.apply(append_trkpt, axis=1, trkseg=trkseg, doc=doc)
+    
+    with open(output_filename, 'w') as fh:
+        doc.writexml(fh, indent=' ')
+
+
+def get_data(in_directory, amenity_schema=amenity_schema):
+    weather = spark.read.json(in_directory, schema=amenity_schema)
+    weather = weather.na.drop(subset=['name'])
+    # weather = weather.drop('timestamp')  # Not used as part of our dataset
+    weather = weather.withColumn('amenity', functions.lower('amenity'))  
+    w = Window.partitionBy('amenity')
+    weather = weather.withColumn('count', functions.count('amenity').over(w))
+    return weather
+
+
+def eda(weather):
+    # Dict format of number of amenities
+    weather = weather.toPandas()
+    amenities_list = set(weather['amenity'])
+    counter_list = Counter(list(weather['amenity'])) 
+    print(counter_list)  # We can see various amenities, mainly restaurants
+
+    # Bar Graph of amenities
+    weather_grouped = weather.groupby('amenity')['amenity'].agg(count='count')
+    ax = weather_grouped.plot(kind='bar', figsize=(10,6), fontsize=8)
+    ax.set_title("Count of Different Amenities in Vancouver")
+    ax.set_ylabel("Count")
+    plt.subplots_adjust(bottom=0.30)
+    # plt.show()
+
+    # Find attractions
+    mask = weather['tags'].apply(lambda x: True if 'tourism' in x else False)
+    weather_attr = weather[mask]
+    
+    print(weather_attr)
+
+
 def main(in_directory, out_directory):
 
-    weather = spark.read.json(in_directory, schema=amenity_schema)
-    weather.show()
+    weather = get_data(in_directory)
+    eda(weather)
 
-    # cleaned_data = weather.filter(weather['qflag'].isNull())
-    # cleaned_data = cleaned_data.filter(cleaned_data['station'].startswith('CA'))
-    # cleaned_data = cleaned_data.filter(cleaned_data['observation'] == 'TMAX')
-    # cleaned_data = cleaned_data.withColumn('tmax', cleaned_data['value'] / 10)
-    #
-    # cleaned_data = cleaned_data.select(
-    #     cleaned_data['station'],
-    #     cleaned_data['date'],
-    #     cleaned_data['tmax'],
-    # )
-    #
-    # cleaned_data.write.json(out_directory, compression='gzip', mode='overwrite')
+    
+    # weather.write.json(out_directory, compression='gzip', mode='overwrite')
 
 
 if __name__ == '__main__':
